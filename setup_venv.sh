d7509b3cf9137c3752b928e1cffbf03f509e2416
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-06-26 20:48:17 +0200

    install analysta in venv setup script

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 virtualenv bearing_venv &&\
 source bearing_venv/bin/activate &&\
-pip install wheel ipykernel &&\
-pip install -r anomaly_detection/requirements.txt &&\
+pip install wheel ipykernel
+
+if $?
+then
+    echo "Error during setup, exiting"
+    exit
+fi
+
+(cd anomaly_detection &&\
+pip install -r requirements.txt &&\
+python setup.py install)
+
+if $?
+then
+    echo "Error during setup, exiting"
+    exit
+fi
+
 ipython kernel install --user --name=bearing_venv
+
+if $?
+then
+    echo "Error during setup, exiting"
+    exit
+fi
 
 echo
 echo "Finished setting up virtualenv under bearing_venv"
