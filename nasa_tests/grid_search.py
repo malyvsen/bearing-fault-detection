c02f9f7aec4ff5637771f8375519e67422d6b020
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-07-01 10:41:29 +0200

    convert grid search values to python ints for serializability

--- nasa_tests/grid_search.py
+++ nasa_tests/grid_search.py
@@ -13,8 +13,10 @@ if not os.path.exists('temp'):
 
 accuracies = {}
 for cells in 2 ** np.arange(4, 9):
+    cells = int(cells) # for serializability
     config['model']['cells'] = cells
     for look_back in 2 ** np.arange(4, 9):
+        look_back = int(look_back) # for serializability
         config['preparation']['look_back'] = look_back
         with open('temp/config.json', 'w') as config_file:
             json.dump(config, config_file)
