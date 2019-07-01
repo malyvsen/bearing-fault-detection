25a130a1a861d7031ffcb96c9a1a69f5df918b02
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-07-02 00:29:55 +0200

    more flexible grid search

--- nasa_tests/grid_search.py
+++ nasa_tests/grid_search.py
@@ -3,31 +3,29 @@ import csv
 import json
 import numpy as np
 from tqdm import tqdm
+from analysta.cli.model import run_single
 
 
+def grid_search(cells_values=2 ** np.arange(4, 9), look_back_values=2 ** np.arange(4, 9)):
+    result = {}
+    with open('lstm_config.json', 'r') as config_file:
+        config = json.loads(config_file.read())
 
-with open('lstm_config.json', 'r') as config_file:
-    config = json.loads(config_file.read())
+    for cells in cells_values:
+        cells = int(cells) # for serializability
+        config['model']['cells'] = [cells]
+
+        for look_back in look_back_values:
+            look_back = int(look_back) # for serializability
+            config['preparation']['look_back'] = look_back
+            with open('temp_config.json', 'w') as config_file:
+                json.dump(config, config_file)
+            report, _, _ = run_single('temp_config.json')
+            result[f'{(cells, look_back)}'] = report['out.model.test.acc']
+    return result
 
 
-accuracies = {}
-for cells in 2 ** np.arange(4, 9):
-    cells = int(cells) # for serializability
-    config['model']['cells'] = [cells]
-
-    for look_back in 2 ** np.arange(4, 9):
-        look_back = int(look_back) # for serializability
-        config['preparation']['look_back'] = look_back
-        with open('temp_config.json', 'w') as config_file:
-            json.dump(config, config_file)
-        os.system('analysta -vv model single -c temp_config.json')
-        for filename in os.listdir('results'):
-            if 'report' not in filename:
-                continue
-            with open('results/' + filename) as report_file:
-                reader = csv.DictReader(report_file)
-                for line in reader:
-                    accuracies[f'{(cells, look_back)}'] = line['out.model.test.acc']
-
-with open('grid_search_results.json', 'w') as results_file:
-    json.dump(accuracies, results_file)
+if __name__ == '__main__':
+    result = grid_search()
+    with open('grid_search_result.json', 'w') as result_file:
+        json.dump(result, results_file)
