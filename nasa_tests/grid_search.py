799a913afe7f10731741a96f105f9fb341a53a81
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-07-01 11:07:33 +0200

    fix naming mistake in grid search

--- nasa_tests/grid_search.py
+++ nasa_tests/grid_search.py
@@ -17,7 +17,7 @@ for cells in tqdm(2 ** np.arange(4, 9)):
     for look_back in tqdm(2 ** np.arange(4, 9)):
         look_back = int(look_back) # for serializability
         config['preparation']['look_back'] = look_back
-        with open('temp/config.json', 'w') as config_file:
+        with open('temp_config.json', 'w') as config_file:
             json.dump(config, config_file)
         results, _, _ = run_single('temp_config.json', results_dir='temp_results')
         accuracies[f'{(cells, look_back)}'] = results['out.model.test.acc']
