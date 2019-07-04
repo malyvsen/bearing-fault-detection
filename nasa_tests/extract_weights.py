bcd9de564c383632a837baa8fd33e0fdafb0de0d
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-07-04 15:40:52 +0200

    fix syntax in extract_weights.py

--- nasa_tests/extract_weights.py
+++ nasa_tests/extract_weights.py
@@ -13,7 +13,7 @@ model.add(Dense(config['preparation']['out_buckets'], activation='softmax'))
 model.load_weights('results_one_channel/model.h5')
 
 
-num_units = int(model.layers[0].weights[0].shape[1]) / 4)
+num_units = int(model.layers[0].weights[0].shape[1] / 4)
 assert num_units == config['model']['cells'][0]
 
 W_x = model.layers[0].get_weights()[0]
@@ -31,10 +31,10 @@ result['W_fh'] = W_h[:, units: units * 2]
 result['W_gh'] = W_h[:, units * 2: units * 3]
 result['W_oh'] = W_h[:, units * 3:]
 
-result['b_i ']= b[:units]
-result['b_f ']= b[units: units * 2]
-result['b_g ']= b[units * 2: units * 3]
-result['b_o ']= b[units * 3:]
+result['b_i '] = b[:units]
+result['b_f '] = b[units: units * 2]
+result['b_g '] = b[units * 2: units * 3]
+result['b_o '] = b[units * 3:]
 
 
 with open('results_one_channel/mdoel_cut.json', 'w') as result_file:
