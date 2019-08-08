dca2c460f2d5a43bd9a0db889aa6a5b7466d8ce2
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-08-08 09:59:18 +0200

    add downsampling functionality to Test

--- nasa_tests/raw_data.py
+++ nasa_tests/raw_data.py
@@ -3,36 +3,56 @@ import csv
 from datetime import datetime
 
 
+
 class Test:
-    def __init__(self, number, dir, num_channels, fault_channels, frequency=20000):
+    def __init__(self, number, dir, num_channels, fault_channels, original_frequency=20000):
         self.number = number
+        self.dir = dir
         self.path = os.path.join('data/IMS', dir)
         self.filenames = sorted(os.listdir(self.path))
-        self.files = [File(self.path, filename) for filename in self.filenames]
+        self.files = [File(self, filename) for filename in self.filenames]
         self.start = self.files[0].datetime
         self.end = self.files[-1].datetime
         self.duration = (self.end - self.start).total_seconds()
         self.num_channels = num_channels
         self.fault_channels = fault_channels
-        self.frequency = frequency
+        self.original_frequency = original_frequency
+        self.frequency = self.original_frequency
+
+
+    def downsampled(self, frequency):
+        assert frequency <= self.original_frequency
+        result = type(self)(self.number, self.dir, self.num_channels, self.fault_channels, self.original_frequency)
+        result.frequency = frequency
+        return result
+
 
     def __iter__(self):
         return iter(self.files)
+
 
     def __len__(self):
         return len(self.files)
 
 
+
 class File:
-    def __init__(self, dir, name):
+    def __init__(self, test, name):
+        self.test = test
         self.name = name
-        self.path = os.path.join(dir, name)
+        self.path = os.path.join(self.test.path, self.name)
         self.datetime = datetime(*[int(x) for x in name.split('.')])
+
 
     def __iter__(self):
         with open(self.path, 'r') as file:
+            frequency_counter = 0
             for row in csv.reader(file, delimiter='\t'):
-                yield [float(x) for x in row]
+                frequency_counter += self.test.frequency
+                if frequency_counter >= self.test.original_frequency:
+                    frequency_counter -= self.test.original_frequency
+                    yield [float(x) for x in row]
+
 
 
 tests = [
