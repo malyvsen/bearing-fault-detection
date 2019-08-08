4506a9d88c1ed8c3af2d697758f8b638b69f54a9
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-08-08 12:25:40 +0200

    add save/load functionality to Test

--- nasa_tests/raw_data.py
+++ nasa_tests/raw_data.py
@@ -1,6 +1,9 @@
 import os
 import csv
+import pickle
+import numpy as np
 from datetime import datetime
+from tqdm import tqdm
 
 
 
@@ -10,7 +13,7 @@ class Test:
         self.dir = dir
         self.path = os.path.join('data/IMS', dir)
         self.filenames = sorted(os.listdir(self.path))
-        self.files = [File(self, filename) for filename in self.filenames]
+        self.files = [File(self, filename, id) for id, filename in enumerate(self.filenames)]
         self.start = self.files[0].datetime
         self.end = self.files[-1].datetime
         self.duration = (self.end - self.start).total_seconds()
@@ -27,6 +30,36 @@ class Test:
         return result
 
 
+    def numpy(self, read_wrapper=tqdm, cache=True):
+        if hasattr(self, 'cached_numpy'):
+            return self.cached_numpy
+        result = []
+        for file in read_wrapper(self.files):
+            result.append([x for x in file])
+        maxlen = max(len(x) for x in result)
+        for x in result:
+            x += [np.nan] * (maxlen - len(x))
+        result = np.array(result)
+        if cache:
+            self.cached_numpy = result
+        return result
+
+
+    def save(self, path=None, read_wrapper=tqdm):
+        if path is None:
+            path = f'data/raw/{self.frequency}hz/{self.number}.pickle'
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        self.numpy(read_wrapper=read_wrapper, cache=True)
+        with open(path, 'wb') as file:
+            pickle.dump(self, file)
+
+
+    @classmethod
+    def load(cls, path):
+        with open(path, 'rb') as file:
+            return pickle.load(file)
+
+
     def __iter__(self):
         return iter(self.files)
 
@@ -37,16 +70,19 @@ class Test:
 
 
 class File:
-    def __init__(self, test, name):
+    def __init__(self, test, name, id):
         self.test = test
         self.name = name
         self.path = os.path.join(self.test.path, self.name)
         self.datetime = datetime(*[int(x) for x in name.split('.')])
+        self.id = id
 
 
     def __iter__(self):
+        frequency_counter = 0
+        if hasattr(self.test, 'cached_numpy'):
+            return iter(self.test.cached_numpy[self.id])
         with open(self.path, 'r') as file:
-            frequency_counter = 0
             for row in csv.reader(file, delimiter='\t'):
                 frequency_counter += self.test.frequency
                 if frequency_counter >= self.test.original_frequency:
